4313e7e869912967ee21768a67111f70d135689c
Zhaofeng Li <hello@zhaofeng.li>
2016-05-16 07:43:03 +0800

    battery.sh: Fix indentation

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -3,25 +3,25 @@ modprobe i2c-dev
 modprobe test_power
 
 
-	#search AXP288 chip on /dev/i2c-dev?
-	for i in `seq 1 12`;
- 	do
-	     RET=$(i2cget -f -y $i 0x34 0|cut -d 'x' -f1)
-	     if [ $RET -eq 0 ]; then 
-	     #echo /dev/i2c-dev$i
-	     I2C_DEV=$i
-	     break	 
-	     else I2C_DEV=-1;   
-        fi
-	done
+#search AXP288 chip on /dev/i2c-dev?
+for i in `seq 1 12`;
+do
+     RET=$(i2cget -f -y $i 0x34 0|cut -d 'x' -f1)
+     if [ $RET -eq 0 ]; then 
+     #echo /dev/i2c-dev$i
+     I2C_DEV=$i
+     break	 
+     else I2C_DEV=-1;   
+fi
+done
 
-	if [ $I2C_DEV -eq -1 ]; then
-	echo "No AXP288 PMIC on i2c Bus"
-	exit 113
-	fi 	
+if [ $I2C_DEV -eq -1 ]; then
+echo "No AXP288 PMIC on i2c Bus"
+exit 113
+fi 	
 
-	# force ADC enable for battery voltage and current
-	i2cset -y -f $I2C_DEV 0x34 0x82 0xC3
+# force ADC enable for battery voltage and current
+i2cset -y -f $I2C_DEV 0x34 0x82 0xC3
 
 
 while true
